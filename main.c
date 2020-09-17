47af00f0ed9a5b6a53fae1eb112a75bf3eff5d00
diii1 <bayu.setiaji709@gmail.com>
2020-09-17 13:24:26 +0800

    First Commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("===========================================\n\n");
+	printf("               BIODATA DIRI   			   \n\n");
+	printf("===========================================\n\n");
+	printf("Nama 		: RAVIY BAYU SETIAJI \n");
+	printf("NPM		: 19081010010 \n");
+	printf("Jurusan 	: Informatika Angakatan 19\n");
+	printf("Jenis Kelamin 	: Laki - Laki \n");
+	printf("Alamat 		: Dsn. Prasung Tani RT 001 / RW 001  \n");
+	printf("		  Ds. Prasung Kec. Buduran Kab. Sidoarjo");
+	
+	return 0;
+}
